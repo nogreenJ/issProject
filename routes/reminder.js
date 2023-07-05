1ffcd302cf18548f6a40bfb948ff19a3ab874dce
nogreenJ <joaoldp135@gmail.com>
2023-07-05 08:52:49 -0300

    Email de teste e deleção de lembretes.

--- routes/reminder.js
+++ routes/reminder.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const schedule = require('node-schedule');
 var nodemailer = require('nodemailer');
+const _ = require('lodash');
 const express = require('express')
 var router = express.Router();
 var path = require('path');
@@ -32,37 +33,60 @@ var transporter = nodemailer.createTransport({
 router.get('/', function (req, res) {
     const data = req.query.data;
     const email = req.query.email;
-    console.log('Criando lembrete para enviar para ' + email + ' em ' + data + '...')
+    if (!email) {
+        console.log('Limpando jobs atuais...')
+        const jobNames = _.keys(schedule.scheduledJobs);
+        for (let name of jobNames) schedule.cancelJob(name);
+    } else if (!data) {
+        console.log('JOBS ATUAIS: ' + _.keys(schedule.scheduledJobs));
+        console.log('Enviando email para ' + email + ' daqui 30 segundos...')
+        const date = new Date(new Date().getTime() + 30000);
 
-    //Cria lembrete na hora inicial
-    let d = data.split('/');
-    const date = new Date(d[1] + '/' + d[0] + '/' + d[2]);
+        mailOptions.subject = 'Email de teste!';
+        mailOptions.text = 'Email de teste!';
 
-    mailOptions.to = email;
-    mailOptions.text = 'A ISS está visível agora!';
-    schedule.scheduleJob(date, function () {
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email do momento enviado para ' + email + ': ' + info.response);
-            }
+        schedule.scheduleJob(date, function () {
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email de teste enviado para ' + email + ': ' + info.response);
+                }
+            });
         });
-    });
+    } else {
+        console.log('Criando lembrete para enviar para ' + email + ' em ' + data + '...')
 
-    //Cria lembrete na hora final
-    mailOptions.text = 'A ISS estará visível hoje a partir das ' + date.getUTCHours() + ":" + date.getUTCMinutes();
-    date.setHours(0, 0, 0, 0);
+        //Cria lembrete na hora inicial
+        let d = data.split('/');
+        const date = new Date(d[1] + '/' + d[0] + '/' + d[2]);
 
-    schedule.scheduleJob(date, function () {
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email do dia enviado para ' + email + ': ' + info.response);
-            }
+        mailOptions.to = email;
+        mailOptions.text = 'A ISS está visível agora!';
+        schedule.scheduleJob(date, function () {
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email do momento enviado para ' + email + ': ' + info.response);
+                }
+            });
         });
-    });
+
+        //Cria lembrete na hora final
+        mailOptions.text = 'A ISS estará visível hoje a partir das ' + date.getUTCHours() + ":" + date.getUTCMinutes();
+        date.setHours(0, 0, 0, 0);
+
+        schedule.scheduleJob(date, function () {
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email do dia enviado para ' + email + ': ' + info.response);
+                }
+            });
+        });
+    }
 
     res.status(200).send();
 })
